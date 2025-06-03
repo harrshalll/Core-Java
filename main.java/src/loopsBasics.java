bca49b73fb62e91e3d9e45c6e69c4605acf2a05f
harrshalll <havhad143@gmail,com>
2025-06-03 17:04:37 +0530

    Basics of loop studied

--- main.java/src/loopsBasics.java
+++ main.java/src/loopsBasics.java
@@ -1,6 +1,16 @@
 public class loopsBasics {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int count = 2;
+        while(count < 20){
+            System.out.println(count);
+            count++;
+        }
+        // for loop
+        int i = 10;
+        for(i = 10; i<=25 ; i++){
+            System.out.println(i);
+        }
+
     }
 }
 
