9ffa5473f77d8f0098e0c71e7806374815555d83
harrshalll <havhad143@gmail.com>
2025-06-23 22:35:27 +0530

    Update FibonacciNumbers.java

--- main.java/src/FibonacciNumbers.java
+++ main.java/src/FibonacciNumbers.java
@@ -3,12 +3,12 @@ public class FibonacciNumbers {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int a = 0;
-        int b = 1;
-        int count = 2;
+        int a = 0; //---> actual fibonacci number
+        int b = 1; //---> actual fibonacci number
+        int count = 2; //----> index of fibonacci number like f(0) = 0, f(1) = 1, f(2) = 1, f(3) = 2
         while(count<=n){
             int temp = b;
-            b = b + a;
+            b = b + a;//----> b updates value after each iteration
             a = temp;
             count++;
         }
