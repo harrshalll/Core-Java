3a147269c881e376aa73c0a066ab4f1413041d6f
harrshalll <havhad143@gmail,com>
2025-06-23 17:30:59 +0530

    Do while loop learned

---
+++ main.java/src/DoWhile.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println("Hello World");
+            i++;
+        }while(i<=3);
+    }
+}
