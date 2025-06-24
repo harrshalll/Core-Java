ee04ee43d558057d9571fc36b1d1bfb944de7f80
harrshalll <havhad143@gmail.com>
2025-06-24 22:07:17 +0530

    Update CountingOccurence.java

--- main.java/src/CountingOccurence.java
+++ main.java/src/CountingOccurence.java
@@ -19,25 +19,4 @@ public class CountingOccurence {
     }
 
 }
-=======
-import java.util.Scanner;
-/*there are two ways to find counting occurence
-1st = convert the datatypes from int to string and iterate over the all char.
-2nd = divide the given number by 10 remainder tells about counting occurence .
- */
-public class CountingOccurence {
-    public static void main(String[] args) {
-       int n = 1223322552;
-       int count = 0;
-       while (n > 0){
-           int remainder = n % 10;
-           if(remainder ==2){
-               count++;
-           }
-           n = n / 10;
-       }
-        System.out.println(count);
-    }
 
-}
->>>>>>> 0f8105fe888e81ecc95f4f35b73d33dba1deb633
