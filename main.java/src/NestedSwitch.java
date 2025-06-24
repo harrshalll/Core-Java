df49fd7683e77c8dfa956d7424c3e40a564f358f
harrshalll <havhad143@gmail,com>
2025-06-24 14:33:26 +0530

    Switch case learned

---
+++ main.java/src/NestedSwitch.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class NestedSwitch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter EmpID:");
+        int empID = in.nextInt();
+        String Department = in.next();
+
+
+        switch(empID){
+            case 1:
+                System.out.println("Harshal Avhad");
+                switch(Department) {
+                    case "IT":
+                        System.out.println("IT Department");
+                        break;
+                    case "HR":
+                        System.out.println("HR Department");
+                        break;
+                    case "Account":
+                        System.out.println("Accounts Department");
+                        break;
+                    default:
+                        System.out.println("Please enter valid Department");
+                }
+                break;
+
+            case 2:
+                System.out.println("Shashank Bankar");
+                switch(Department) {
+                    case "IT":
+                        System.out.println("IT Department");
+                        break;
+                    case "HR":
+                        System.out.println("HR Department");
+                        break;
+                    case "Account":
+                        System.out.println("Accounts Department");
+                        break;
+                    default:
+                        System.out.println("Please enter valid Department");
+                }
+                break;
+            case 3:
+                System.out.println("Elon Musk");
+                switch(Department) {
+                    case "IT":
+                        System.out.println("IT Department");
+                        break;
+                    case "HR":
+                        System.out.println("HR Department");
+                        break;
+                    case "Account":
+                        System.out.println("Accounts Department");
+                        break;
+                    default:
+                        System.out.println("Please enter valid Department");
+                }
+                break;
+            default:
+                System.out.println("Please Enter Correct empID");
+        }
+
+
+    }
+}
---
+++ main.java/src/SwitchCases.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SwitchCases {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Fruit Name:");
+        String fruit = in.next();
+
+       switch(fruit){
+           case "Mango":
+               System.out.println("King of Fruits");
+               break;
+           case "Apple":
+               System.out.println("A sweet red fruits");
+               break;
+           case "Orange":
+               System.out.println("A round orange fruit");
+               break;
+           default:
+               System.out.println("Invalid Case");
+       }
+
+    }
+}
+/* note:
+1.cases have to be same type as expression(here string)
+2.duplicate case values are not allowed.
+3.default will execute when none of the above does.
+4if default is not at the end, put the break after to terminate the program. */
+
+
