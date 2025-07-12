6948a58f76d3d765b9b3d9731a3454012737ada2
harrshalll <havhad143@gmail,com>
2025-07-12 17:33:05 +0530

    2D Array

--- main.java/src/MultidimensionalArray.java
+++ main.java/src/MultidimensionalArray.java
@@ -1,6 +1,31 @@
 import java.util.Arrays;
 public class MultidimensionalArray {
     public static void main(String[] args) {
+        int[][] arr = new int[2][2];
+        int[][] nums = {{1,2,3},
+                        {4,5,6,}};
 
     }
 }
+/*
+NOTES
+[ STACK ]                               [ HEAP ]
+-----------                             ----------------------------------
+arr ─────┐                              arr --> [ ─────┬────── ]
+         │                                        ↓         ↓
+         └───────────────────────────────────▶ [1, 2, 3]   [4, 5, 6]
+                                             (arr[0])    (arr[1])
+
+arr[0] = [1, 2, 3]
+            ↑
+         arr[0][0] = 1
+         arr[0][1] = 2
+         arr[0][2] = 3
+
+arr[1] = [4, 5, 6]
+            ↑
+         arr[1][0] = 4
+         arr[1][1] = 5
+         arr[1][2] = 6
+
+ */
